2d5f7f21fb672914c3770a1b37b0020e84b3232f
Héber Lima Silva <heberlimasilva@gmail.com>
2022-02-15 18:04:01 -0300

    Second Commit

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,17 @@
 import { NextPage } from "next"
+import Head from "next/head";
 
 const IndexPage:NextPage = () => {
   return (
-   <h1>Hello World</h1>
+    <>
+      <Head>
+        <title>Início | CT JUNIOR</title>
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </>
+    
   )
 };
 

e55cec3f2c0aebf9dd77f38f19d276eafac844f8
Héber Lima Silva <heberlimasilva@gmail.com>
2022-02-18 13:33:44 -0300

    Section 'POR QUE A CT JUNIOR' criada

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { NextPage } from "next"
+import { NextPage } from "next";
 import Head from "next/head";
 
 import { Header } from "../components/Header";
 
-import Styles from '../styles/home.module.scss'
+import Styles from "../styles/home.module.scss";
 
-const IndexPage:NextPage = () => {
+const IndexPage: NextPage = () => {
   return (
     <>
       <Head>
@@ -13,10 +13,14 @@ const IndexPage:NextPage = () => {
       </Head>
 
       <Header />
-      
+
       <main>
         <section className={Styles.sectionInitial}>
-          <h1>Soluções em<br/><b>Tecnologia Construção Civil</b></h1>
+          <h1>
+            Soluções em
+            <br />
+            <b>Tecnologia Construção Civil</b>
+          </h1>
         </section>
         <section className={Styles.servicesSection}>
           <div className={Styles.servicesContent}>
@@ -26,30 +30,49 @@ const IndexPage:NextPage = () => {
             <div className={Styles.servicesCard}>
               <img src="./images/logoTecnologia.svg" alt="" />
               <h3>TECNOLOGIA</h3>
-              <p>Desenvolvemos sites institucionais, e-commerces e sistemas web com suporte a todos os dispositivos móveis.
-Trabalhamos também com protótipos de aplicativos.</p>
+              <p>
+                Desenvolvemos sites institucionais, e-commerces e sistemas web
+                com suporte a todos os dispositivos móveis. Trabalhamos também
+                com protótipos de aplicativos.
+              </p>
             </div>
             <div className={Styles.servicesCard}>
               <img src="./images/logoEngenharia.svg" alt="" />
               <h3>CONSTRUÇÃO CIVIL</h3>
-              <p>Realizamos projetos elétricos, arquitetônicos e hidrossanitários, projetos de piscina e de captação de água da chuva.
-Trabalhamos também com e regularização, usucapião de imóveis.</p>
-
+              <p>
+                Realizamos projetos elétricos, arquitetônicos e
+                hidrossanitários, projetos de piscina e de captação de água da
+                chuva. Trabalhamos também com e regularização, usucapião de
+                imóveis.
+              </p>
             </div>
-            {/* <a>Solicitar um Diagnóstico</a> */}
             <button className={Styles.requestDiagnosis}>
               Solicitar um Diagnóstico
             </button>
           </div>
-          
         </section>
-       
-
+        <section className={Styles.whyCtjuniorSection}>
+            <div className={Styles.whyCtjuniorContent}>
+                <div className={Styles.whyCtjuniorTitle}> 
+                    <h2>Por que a CT Junior</h2>
+                </div>
+                <p>Entendemos suas necessidades e construímos seus sonhos a partir de nossos projetos.</p>
+                <ul>
+                    <li>Somos uma instituição sem fins lucrativos, formada por jovens voluntários da maior universidade do Espírito Santo, e oferecemos serviço de excelência com ótimo custo benefício.</li>
+                    <li>A CT Junior se encontra no maior Centro Tecnológico do Espírito Santo, contando com apoio de professores e alunos renomados dessa instituição de elite.</li>
+                    <li>Estamos há mais de 27 anos no mercado, apresentando soluções para clientes de diversos setores. Todo nosso faturamento é reinvestido na profissionalização dos estudantes voluntários que participam do projeto.</li>
+                </ul>
+                <img src={'/images/studentsWorking.png'} alt="" />
+            </div>
+        </section>
+        <section className={Styles.partnersSection}>
+            <div className={Styles.partnersTitle}>
+                <h2>Nossos Parceiros</h2>
+            </div>
+        </section>
       </main>
-      
     </>
-    
-  )
+  );
 };
 
 export default IndexPage;

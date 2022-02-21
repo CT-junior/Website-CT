6d9d2da64bc9deced32624f853196a9e969d7dd4
faakit <andrealtosan@gmail.com>
2022-02-21 20:42:54 -0300

    Adicionado tipos e indice ao back-end

---
+++ backend/src/api/trabalho-portfolio/controllers/trabalho-portfolio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  trabalho-portfolio controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::trabalho-portfolio.trabalho-portfolio');
---
+++ backend/src/api/trabalho-portfolio/routes/trabalho-portfolio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * trabalho-portfolio router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::trabalho-portfolio.trabalho-portfolio');

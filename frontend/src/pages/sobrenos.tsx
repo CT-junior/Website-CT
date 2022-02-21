0a4fa0668bdf1f28ec1f61f5adcb78b42ab801cc
Héber Lima Silva <heberlimasilva@gmail.com>
2022-02-21 19:54:46 -0300

    Criando nova pagina

---
+++ frontend/src/pages/sobrenos.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+
+const SobreNosPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Sobre Nós | CT JUNIOR</title>
+      </Head>
+      <Header />
+    <main>
+        oioidoiadoadakhdgvaidujafsvdjka
+    </main>
+      <Footer/>
+    </>
+  );
+};
+
+export default SobreNosPage;

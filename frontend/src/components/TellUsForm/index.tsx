c04bfb9dfe5a3e09b065fcd9a9439a9805a89b20
Héber Lima Silva <heberlimasilva@gmail.com>
2022-02-22 17:53:36 -0300

    Atualizando deslizante do formulário da home

--- frontend/src/components/TellUsForm/index.tsx
+++ frontend/src/components/TellUsForm/index.tsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Styles from "./styles.module.scss";
 
 export function TellUsForm() {
 
-  const [rangeValue, setRangeValue] = useState(5)
-  
+  const [rangeValue, setRangeValue] = useState(0)
+  const [priceRange, setPriceRange] = useState('Até 10k')
+
+  useEffect(() => {
+    if(rangeValue <= 10){
+      setPriceRange('Até 10k')
+    }else if(rangeValue <=20){
+      setPriceRange('Entre 10k e 20k')
+    }else if(rangeValue <=30){
+      setPriceRange('Entre 20k e 30k')
+    }else if(rangeValue < 40){
+      setPriceRange('Entre 30k e 40k')
+    }else{
+      setPriceRange('Mais de 40k')
+    }
+  }); 
+
   return (
     <form action="" className={Styles.tellUsForm}>
       <fieldset>
@@ -27,19 +42,15 @@ export function TellUsForm() {
         <div className={Styles.sliderContainer}>
           <label htmlFor="investimento">Quanto você pretende investir?</label>
           <div className={Styles.slideField}>
-            <input type="range" id="investimento" min="0" max="10.00"  onChange={event =>{setRangeValue(Number(event.target.value))}} />
-            <div className={Styles.rangeValue}>{rangeValue}k</div>
-
+            <input type="range" id="investimento" min="0" max="40.00"  onChange={event =>{setRangeValue(Number(event.target.value))}}  value={rangeValue}/>
+            <div className={Styles.sliderTracinhos}>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
           </div>
-
-          <div className={Styles.sliderTracinhos}>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-          
-
+          <div className={Styles.rangeValue}>{priceRange}</div> 
         </div>
         
         <label htmlFor="ideia">Descreva sua ideia :)</label>

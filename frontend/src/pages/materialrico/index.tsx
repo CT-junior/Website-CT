70fa791d2ff7cb2974a8d578699ed13b539e3ef7
Héber Lima Silva <heberlimasilva@gmail.com>
2022-03-15 22:31:39 -0300

    Ajustando página material rico

--- frontend/src/pages/materialrico/index.tsx
+++ frontend/src/pages/materialrico/index.tsx
@@ -7,41 +7,45 @@ import Styles from "./styles.module.scss";
 export default function MaterialRico() {
   return (
     <main>
-       <Head>
-        <title>Material Rico 
-          | CT JUNIOR</title>
+      <Head>
+        <title>Material Rico | CT JUNIOR</title>
       </Head>
       <section className={Styles.container}>
         <TitleOrange>MATERIAL RICO</TitleOrange>
         <p>Aproveite nosso contúdo gratuito disponível para download</p>
-        <div className={Styles.materialCard}>
+        <div className={Styles.materialCards}>
+          <div className={Styles.materialCard}>
             <img src="/images/materialRico.png" alt="" />
-            <span>Indicadores Financeiros: dicas para implementar em sua empresa</span>
+            <span>
+              Indicadores Financeiros: dicas para implementar em sua empresa
+            </span>
             <ButtonOrange> Baixar</ButtonOrange>
-        </div>
-        <div className={Styles.materialCard}>
+          </div>
+          <div className={Styles.materialCard}>
             <img src="/images/materialRico.png" alt="" />
             <span>Conheça o E-commerce feito pela CT</span>
             <ButtonOrange> Baixar</ButtonOrange>
-        </div>
-        <div className={Styles.materialCard}>
+          </div>
+          <div className={Styles.materialCard}>
             <img src="/images/materialRico.png" alt="" />
             <span>Entendendo as etapas de um projeto elétrico</span>
             <ButtonOrange> Baixar</ButtonOrange>
-        </div>
-        <div className={Styles.materialCard}>
+          </div>
+          <div className={Styles.materialCard}>
             <img src="/images/materialRico.png" alt="" />
-            <span>Planejamento estratégico: alcance resultados incríveis com essa ferramenta</span>
+            <span>
+              Planejamento estratégico: alcance resultados incríveis com essa
+              ferramenta
+            </span>
             <ButtonOrange> Baixar</ButtonOrange>
-        </div>
-        <div className={Styles.materialCard}>
+          </div>
+          <div className={Styles.materialCard}>
             <img src="/images/plangastos.png" alt="" />
             <span>Planilha de gestão de gastos</span>
             <ButtonOrange> Baixar</ButtonOrange>
+          </div>
         </div>
-        
       </section>
-      
     </main>
   );
 }

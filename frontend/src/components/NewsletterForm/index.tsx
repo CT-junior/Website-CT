d7cbd88cc2db9d8ffc447080da1c2a14db1af3d8
henrycky <henrycky_wottikosky@hotmail.com>
2022-03-18 19:43:56 -0300

    NewsletterForm component

--- frontend/src/components/NewsletterForm/index.tsx
+++ frontend/src/components/NewsletterForm/index.tsx
@@ -8,7 +8,7 @@ export function NewsletterForm(){
                     <h2>Quer receber<br />nossos conteúdos?</h2>
                 </div>
 
-                <form className={styles.newsletterForm}>
+                <form className={styles.newsletterForm} action="javascript:void(0)" method="post">
                     <label htmlFor="name"></label>
                     <input type="text" name="name" id="name" placeholder="Nome" required />
 
@@ -16,7 +16,7 @@ export function NewsletterForm(){
                     <input type="email" name="email" id="email" placeholder="E-mail" required />
 
                     <div className={styles.newsletterButton}>
-                            <button type="submit">Assinar Newsletter</button>
+                        <button type="submit">Assinar Newsletter</button>
                     </div>
                 </form>
             </div>

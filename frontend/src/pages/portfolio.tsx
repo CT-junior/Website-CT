6cbb9d40c2c0086a9eede2565203a00d292beda0
alekswheeler <alexskntks@outlook.com>
2022-10-04 23:07:50 -0300

    textos e imagens dos cases

--- frontend/src/pages/portfolio.tsx
+++ frontend/src/pages/portfolio.tsx
@@ -17,28 +17,34 @@ type ModalData = {
 }
 
 const modalDataSample1: ModalData = {
-  imgSrc: "/images/dualEng.png",
-  title: "SITE INSTITUCIONAL Dual Engenharia",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa.Pellentesque habitant morbi tristique senectus et netus et malesuadafames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor  et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut  adipiscing."
+  imgSrc: "/images/rewaSupply.png",
+  title: "ECCOMERCE REWA SUPPLY",
+  text: "Nesse projeto nossa empresa foi contratada para desenvolver o e-commerce de uma loja de roupas de Vila Velha. Tendo sido lançado não só no Brasil, mas também na Austrália e Estados Unidos, o e-commerce em questão possibilitou um aumento nas vendas da loja, além de uma extensão de seu alcance e uma estética muito mais agradável para o site, que é totalmente acessível seja por celular ou computador."
 }
 
 const modalDataSample2: ModalData = {
   imgSrc: "/images/sindipesMockup.png",
   title: "SITE INSTITUCIONAL SINDPES",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa.Pellentesque habitant morbi tristique senectus et netus et malesuadafames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor  et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut  adipiscing."
+  text: "SINDPES (Sindicato dos Psicanalistas do Espírito Santo) buscava através do website um meio para aumento do alcance da marca juntamente como uma forma de estar expressando as atividades e as pessoas as quais atuam e fazem parte das discussões. Diante disso, realizou-se um site com uma boa navegabilidade e desing agradável, garantindo uma expansão do grupo e uma afirmação conjunta."
 }
 
 
 const modalDataSample3: ModalData = {
   imgSrc: "/images/eurolog.png",
   title: "SITE INSTITUCIONAL Eurolog",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa.Pellentesque habitant morbi tristique senectus et netus et malesuadafames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor  et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut  adipiscing."
+  text: "A EUROLOG, tinha como objetivo desenvolver por completo um site institucional, buscando maior alcance e visibilidade na internet, com o intuito de fortalecer ainda mais a marca no mercado. O objetivo da cliente foi alcançado com sucesso."
 }
 
 const modalDataSample4: ModalData = {
-  imgSrc: "/images/pcMil.png",
-  title: "SITE INSTITUCIONAL PC MIL",
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa.Pellentesque habitant morbi tristique senectus et netus et malesuadafames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor  et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut  adipiscing."
+  imgSrc: "/images/bbuttonLogo.png",
+  title: "WEB SITE BBUTTON",
+  text: "A Bbutton tinha como objetivo criar um site institucional, para aumentar seu alcance, com o desafio de desenvolver um site que facilitasse a manutenção posteriormente. Entregamos um site que atendesse todas as demandas da cliente."
+}
+
+const modalDataSample9: ModalData = {
+  imgSrc: "/images/corecon.png",
+  title: "SITE INSTITUCIONAL CORECON-ES",
+  text: "Atendendo também instituições estaduais, nesse projeto nos foi solicitado a criação de um site institucional para a CORECON. Com nossos serviços o site passou a ser completamente responsivo (que se adapta a qualquer tamanho de janela) e focado na divulgação e fácil manipulação de todas as informações relevantes à instituição."
 }
 
 const modalDataSample5: ModalData = {
@@ -114,7 +120,7 @@ export default function Portifolio() {
 
                 >
                   <SwiperSlide onClick={() => handleOpenModal(modalDataSample1)} className={Styles.card}>
-                    <img src="/images/dualEng.png" alt="" />
+                    <img src="/images/rewaSupply.png" alt="" />
                   </SwiperSlide>
                   <SwiperSlide onClick={() => handleOpenModal(modalDataSample2)} className={Styles.card}>
                     <img src="/images/sindpes.png" alt="" />
@@ -123,7 +129,10 @@ export default function Portifolio() {
                     <img src="/images/eurolog.png" alt="" />
                   </SwiperSlide>
                   <SwiperSlide onClick={() => handleOpenModal(modalDataSample4)} className={Styles.card}>
-                    <img src="/images/pcMil.png" alt="" />
+                    <img src="/images/bbuttonIcon.png" alt="" />
+                  </SwiperSlide>
+                  <SwiperSlide onClick={() => handleOpenModal(modalDataSample9)} className={Styles.card}>
+                    <img src="/images/corecon.png" alt="" />
                   </SwiperSlide>
                 </Carousel>
               </div>
